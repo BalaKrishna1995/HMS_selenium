7a338e9f22b85d416a9be9297f1264d6387ff181
Vicky <Vicky@Vicky>
2024-01-09 11:08:35 +0400

    BuildB01

---
+++ zzHMS/src/main/java/zzHMS/SampleJDBC.java
@@ -0,0 +1,30 @@
+package zzHMS;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SampleJDBC 
+{
+	public static void main(String[] args) throws SQLException 
+	{
+		String url = "jdbc:mysql://localhost:3306/CarDekho";
+		String un = "root";
+		String psw = "Vicky@1234";
+		
+		Connection conn = DriverManager.getConnection(url, un, psw);
+		Statement state = conn.createStatement();
+		ResultSet result = state.executeQuery("select* from facebook1;");
+		
+		while (result.next()) 
+		{
+			String username = result.getString("UN");
+			String pw = result.getString("PSW");
+			
+			System.out.println(username+" "+pw);
+		}		
+		
+	}
+}
